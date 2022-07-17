b0904d7cec01229f59766a2bb88045eea87707ca
Alex Di Maria <alessandro@sophany.it>
2022-07-17 19:01:35 +0200

    feat: defining routes

---
+++ src/views/LogInView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    LogIn
+  </div>
+</template>
+
+<script setup lang="ts"></script>
---
+++ src/views/SignUpView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    SignUp
+  </div>
+</template>
+
+<script setup lang="ts"></script>
